227de8bb92ef0064b2335d4af29dabb2225e79bf
itchonib <itchoib@gmail.com>
2022-01-04 16:17:38 -0600

    test maintenance, updated to relfect latest class names and properties

--- tests/unit/ProductCard.spec.js
+++ tests/unit/ProductCard.spec.js
@@ -5,11 +5,11 @@ import ProductCard from "@/components/ProductCard.vue";
 describe("ProductCard.vue", () => {
   it("renders props when passed", async () => {
     const product = {
-      BasePrice: "14",
+      FormattedPrice: "14.00",
     };
     const wrapper = shallowMount(ProductCard, {
       props: { product }
     });
-    expect(wrapper.find('.product-price').text()).toContain(product.BasePrice);
+    expect(wrapper.find('.product-card__price').text()).toContain(product.FormattedPrice);
   });
 });
