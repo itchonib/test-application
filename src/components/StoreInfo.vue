f0554f9a75ea0d9b83d036e416f624c15651cf1e
itchonib <itchoib@gmail.com>
2022-01-06 17:13:00 -0600

    added hrefs to links

--- src/components/StoreInfo.vue
+++ src/components/StoreInfo.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="store-info">
     <h2 class="store-info__name">{{ salesRep.CompanyName }}</h2>
+    <p>SalesRep Contact: </p>
     <div class="store-info__contact">
       <p class="store-info__label">name:</p>
       <p>{{ salesRep.FirstName + " " + salesRep.LastName }}</p>
       <p class="store-info__label">email:</p>
-      <p>{{ salesRep.EmailAddress }}</p>
+      <a :href="'mailto:' + salesRep.EmailAddress">{{ salesRep.EmailAddress }}</a>
       <p class="store-info__label">phone:</p>
-      <p>{{ salesRep.Phone }}</p>
+      <a :href="'tel:+' + salesRep.phone">{{ salesRep.Phone }}</a>
     </div>
   </div>
 </template>
@@ -32,6 +33,7 @@ export default {
   &__name {
     font-weight: 500;
     @include yellow-underline;
+    margin-bottom: 1rem;
   }
 
   &__contact {
